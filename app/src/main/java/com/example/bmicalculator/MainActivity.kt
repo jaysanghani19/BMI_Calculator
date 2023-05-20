2926f4c6807cfe5fc8b116bccf15858b06595f5b
jaysanghani19 <jaysanghani0@gmail.com>
2023-05-20 09:35:26 +0530

    Crash Handling

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,102 +1,89 @@
 package com.example.bmicalculator
 
-import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var sf : SharedPreferences
+    private lateinit var sf: SharedPreferences
     private lateinit var sfEditor: SharedPreferences.Editor
 
-    private lateinit var height : EditText
-    private lateinit var weight : EditText
+    private lateinit var height: EditText
+    private lateinit var weight: EditText
 
-    private lateinit var bmi : TextView
-    private lateinit var statment : TextView
+    private lateinit var bmi: TextView
+    private lateinit var statment: TextView
 
     private lateinit var calculateButton: Button
 
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sf = getSharedPreferences("Shared Preferece", MODE_PRIVATE)
+        sf = getSharedPreferences("Shared Preference", MODE_PRIVATE)
         sfEditor = sf.edit()
-        height= findViewById(R.id.edittext_height)
-        weight=findViewById(R.id.edittext_weight)
+        height = findViewById(R.id.edittext_height)
+        weight = findViewById(R.id.edittext_weight)
 
         bmi = findViewById(R.id.textview_bmi_result)
         statment = findViewById(R.id.textview_healthy)
 
         calculateButton = findViewById(R.id.button_calculate)
         calculateButton.setOnClickListener {
-            if (height.text.toString() =="" ||weight.text.toString() ==""){
-                Toast.makeText(this, "Enter the weight and height both", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                calculateBMI(weight.text.toString().toDouble() , height.text.toString().toDouble())
-            }
+                calculateBMI(weight.text.toString(), height.text.toString())
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    fun calculateBMI(weight : Double, height:Double)
-    {
-        val height2 = height/100
-        val bmiValue : Double= weight/(height2*height2)
-        bmi.text = bmiValue.toString()
+    fun setBMI(BMI: Float) {
+        bmi.text = BMI.toString()
 
-        if(bmiValue<25 && bmiValue>18.5)
-        {
+        if (BMI < 25 && BMI > 18.5) {
             statment.text = "You're Healthy"
             statment.setTextColor(Color.GREEN)
-        }
-        else{
+        } else {
             statment.text = "You're Not Healthy"
             statment.setTextColor(Color.RED)
         }
+    }
 
+    private fun calculateBMI(weight: String, height: String) {
+        if (weight =="" || height=="")
+        {
+            Toast.makeText(this, "Enter Both Weight and Height", Toast.LENGTH_SHORT).show()
+        }
+        else{
+        val height2 : Float = height.toFloat()/100
+        val bmiValue: Float = weight.toFloat() / (height2 * height2)
+        setBMI(bmiValue)
+        }
     }
 
     override fun onPause() {
         super.onPause()
         sfEditor.apply {
-            putString("Weight",weight.text.toString())
-            putString("Height",height.text.toString())
-            putString("bmi",bmi.text.toString())
+            putString("Weight", weight.text.toString())
+            putString("Height", height.text.toString())
+            putFloat("bmi", bmi.text.toString().toFloat())
             apply()
         }
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onResume() {
         super.onResume()
-        val tempHeight = sf.getString("Height",null)
-        val tempWeight = sf.getString("Weight",null)
-        val tempBMI = sf.getString("bmi",null)
+        val tempHeight = sf.getString("Height", " ")
+        val tempWeight = sf.getString("Weight", " ")
+        val tempBMI : Float= sf.getFloat("bmi", 0.0f)
 
         height.setText(tempHeight)
         weight.setText(tempWeight)
-        bmi.text = tempBMI
-
-        if(tempBMI.toString().toDouble()<25 && tempBMI.toString().toDouble()>18.5)
-        {
-            statment.text = "You're Healthy"
-            statment.setTextColor(Color.GREEN)
-        }
-        else{
-            statment.text = "You're Not Healthy"
-            statment.setTextColor(Color.RED)
-        }
-
+        setBMI(tempBMI)
     }
 
 }
